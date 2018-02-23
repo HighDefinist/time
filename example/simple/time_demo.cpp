3639f4a095cc40fa4ce9b17fa1ce00c3c4f223a2
Solenism <martinzintl+github@gmail.com>
2018-02-23 09:08:28 +0100

    Update time_demo.cpp
    Improved comments.

--- example/simple/time_demo.cpp
+++ example/simple/time_demo.cpp
@@ -1,30 +1,27 @@
 #include "mz/range.hpp"
 #include "mz/time.h"
-//#include "mz/base1.h"
 using namespace std;
 using namespace std::mz;
 
 int main() {
-  // Measure the duration of something
   long long a = 0;
   {
-    auto t1=bench1([&] { // Execute once
+    auto t1=bench1([&] { // Execute once, and return elapsed time in seconds
       for(auto i:range(1000))a += i;
     });
     printf("bench1: %gns\n",t1*1e9);
-    auto tN = benchN(10,[&] { // Execute N times and average
+    auto tN = benchN(10,[&] { // Execute N times and return average execution time
       for(auto i:range(1000))a += i;
     });
     printf("benchN: %gns\n",tN*1e9);
-    auto tT = benchT(0.5,[&] { // Execute until time "t" is elapsed
+    auto tT = benchT(0.5,[&] { // Execute until time "t" is elapsed and return average execution time
       for(auto i:range(1000))a += i;
     });
     printf("benchT: %gns\n",tT*1e9);
   }
-
-   // Benchmarking: Average over multiple separate executions
+   
   {
-    Tbench b;
+    Tbench b; // Benchmarking: Average over multiple separate executions. This class also stores the fastest, the slowest, and last execution time.
     for(auto i:range(1000)) {
       b.Run([&] {
         for(auto i:range(1000))a += i;
