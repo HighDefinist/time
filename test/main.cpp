0d10475515171041167abc09d9c5c7dc26fd5509
loki <loki@localhost.localdomain>
2018-03-26 20:34:50 +0200

    New testing method; included some tests

--- test/main.cpp
+++ test/main.cpp
@@ -7,5 +7,20 @@ using namespace std::mz;
 #include "catch.hpp"
 
 TEST_CASE("time main", "Main functionality") {
+  SECTION("Benchmarks"){
+    long long a=0;
+    auto t1=bench1([&] { // Execute once, and return elapsed time in seconds
+      a+=1;
+    });
+    REQUIRE(a==1);
+    auto tN=benchN(10,[&] { // Execute once, and return elapsed time in seconds
+      a+=1;
+    });
+    REQUIRE(a==11);
+    auto tT=benchT(0.1,[&] { // Execute once, and return elapsed time in seconds
+      a+=1;
+    });
+    REQUIRE(a>11);
+  }   
 }
 
